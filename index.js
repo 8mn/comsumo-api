a42cdbfac5ac108387c1b203dae8f2cbbff7554c
calmighted <manishh@tutanota.com>
2022-02-17 21:29:50 +0530

    dummy dotenv

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+import dotenv from "dotenv"
+import mongoose from "mongoose";
 
-const app = express()
+const app = express();
 
-const PORT = 5000
-
+const PORT = 5000;
 
 let corsOption = {
 	origin: `http://localhost:${PORT}`,
 };
 
 
-app.use(cors(corsOption))
-
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+dotenv.config()
 
 
-app.get("/", (req,res) => {
-    res.json({message:"Welcome to consumo api"})
-})
+
+app.use(cors(corsOption));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+	res.json({ message: "Welcome to consumo api" });
+});
+
+
+
+
+app.listen(PORT, () =>
+	console.log(`Server running on http://localhost:${PORT}`)
+);
+
+
+
+mongoose
+	.connect(process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("COnnected to the database!");
+	})
+	.catch((err) => {
+		console.log("Cannot connect to the database", err);
+		process.exit();
+	});
 
 /*
 
@@ -35,7 +58,3 @@ api/delete/:id  DELETE remove a content by id
 */
 
 
-
-app.listen(PORT, () =>
-	console.log(`Server running on http://localhost:${PORT}`)
-);
